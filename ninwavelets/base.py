b63cc2e4cf226b87bbebf8ce43f0b6bbad981a34
uesseu <sheepwing@kyudai.jp>
2020-08-22 08:54:26 +0900

    Plot when cuda enable was modified
    flip function is differ from that of numpy

--- ninwavelets/base.py
+++ ninwavelets/base.py
@@ -270,7 +270,7 @@ class WaveletBase:
         if self.mode in [CWTMode.Fast]:
             t = self._setup_trans_shape(real_length, real_length)
             result = formula(t, freq)
-            return result / self.get_wavelet_norm(ncp.fft.ifft(result), (1,))
+            return result / self.get_wavelet_norm(ncp.fft.ifft(result), (0,))
         else:
             wavelet = self.make_wavelet(freq)
             half = int((self.sfreq * self.real_wave_length
@@ -451,7 +451,7 @@ class WaveletBase:
                 wavelet = ifft(self.trans_formula(timeline))
             half = int(wavelet.shape[0])
             start, stop = half // 2, half // 2 * 3
-            total_wavelet = ncp.hstack((ncp.conj(ncp.flip(wavelet)), wavelet))
+            total_wavelet = ncp.hstack((ncp.conj(ncp.flip(wavelet, 0)), wavelet))
             wavelet = total_wavelet[start: stop]
             wavelet /= self.get_wavelet_norm(wavelet)
         else:
