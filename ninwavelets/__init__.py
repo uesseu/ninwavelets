09a3991cc6839e1043cbe2a40cdd3a7ade84528c
uesseu <sheepwing@kyudai.jp>
2021-01-17 22:52:24 +0900

    Error was removed

--- ninwavelets/__init__.py
+++ ninwavelets/__init__.py
@@ -1,4 +1,4 @@
 from .base import (WaveletBase, CWTMode, plot_tf, Baseline, np2cp,
-                   WaveletFormula, cp2np)
+                   WaveletFormula)
 from .wavelets import Morse, Morlet, Haar, MexicanHat, Shannon
 from .factor import factor
